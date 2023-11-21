933b035ce01ee2f1078075986d4fb2937ea25169
yusfmre <yusfmre0@gmail.com>
2023-11-22 01:28:44 +0300

    responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ import Background from "./components/Background"
 import Skills from './components/Skills'
 import Footer from "./components/Footer"
 
+export const metadata = {
+  title: 'yusfmre.',
+  author: 'yusfmre.',
+  publisher: 'yusfmre.',
+  description: 'Personal website for contact with yusfmre.'
+}
+
 export default function Home() {
   return (
     <main className="px-72 max-[1100px]:px-40 max-[700px]:px-20 max-[500px]:px-5">
